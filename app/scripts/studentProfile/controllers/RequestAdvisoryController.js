bfa5b1e419a5151897ccb0b349945553f3ec7f3d
daprieto1 <da.prieto1@uniandes.edu.co>
2017-04-28 18:44:15 +0000

    begin notifications

--- app/scripts/studentProfile/controllers/RequestAdvisoryController.js
+++ app/scripts/studentProfile/controllers/RequestAdvisoryController.js
@@ -3,6 +3,27 @@
     angular.module('studentProfileModule')
         .controller('RequestAdvisoryController', function ($scope) {
             var vm = this;
+            
+            vm.addSession = () => {
+              vm.sessions.push({});  
+            };
+            
+            vm.deleteSession = index => {
+                vm.sessions.splice(index,1);                
+            };
+            
+            function initMap() {
+                var address = { lat: 4.652657, lng: -74.063915 };
+                vm.map = new google.maps.Map(document.getElementById('map'), {
+                    zoom: 16,
+                    center: address,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                });
+                var marker = new google.maps.Marker({
+                    position: address,
+                    map: vm.map
+                });
+            }
 
             function initCtrl() {
                 vm.numberStudentsSlider = {
@@ -14,6 +35,20 @@
                         showTicksValues: true
                     }
                 };
+                
+                vm.numberHoursSlider = {
+                    value: 2,
+                    options: {
+                        floor: 2,
+                        ceil: 8,
+                        showTicks: true,
+                        showTicksValues: true
+                    }
+                };
+                
+                vm.sessions = [{}];
+                
+                initMap();
             }
 
             initCtrl();
