33aedaae0bc80a01b3e47f65369e1f6cd96fb755
daprieto1 <da.prieto1@uniandes.edu.co>
2017-05-28 16:27:45 -0500

    improve design for services history

--- app/scripts/studentProfile/controllers/StudentServiceHistoryController.js
+++ app/scripts/studentProfile/controllers/StudentServiceHistoryController.js
@@ -4,7 +4,12 @@
         .controller('StudentServiceHistoryController', function ($scope, $cookies, AdvisoryServiceServices, SESSION_STATES) {
             var vm = this;
 
-            vm.selectService = service => {                             
+            vm.sortBy = (propertyName) => {
+                vm.reverse = (vm.propertyName === propertyName) ? !vm.reverse : false;
+                vm.propertyName = propertyName;
+            };
+
+            vm.selectService = service => {
                 if (vm.selectedService === service) {
                     vm.selectedService = undefined;
                 } else {
@@ -14,16 +19,18 @@
 
             function initCtrl() {
                 vm.selectedService = undefined;
+                vm.propertyName = 'createdAt';
+                vm.reverse = false;
 
                 AdvisoryServiceServices.getAllByStudentId($cookies.get('userId'))
-                    .then(services=>{
+                    .then(services => {
                         vm.services = services.map(service => {
                             service.createdAt = moment(service.createdAt).format('LL');
                             service.sessions = service.sessions.map(session => {
                                 session.startDateToShow = moment(session.startDate).format('LL');
-                                session.numHours = session.duration / 60; 
+                                session.numHours = session.duration / 60;
                                 session.state = SESSION_STATES.find(state => {
-                                   return state.id === session.state;
+                                    return state.id === session.state;
                                 });
                                 return session;
                             });
